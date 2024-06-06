ea4f9470492d3fd394892de72fa829e925f97318
Andre Braga <andre-az@hotmail.com>
2024-06-05 21:14:43 -0300

    Primeiro commit de verdade

--- criar_db.py
+++ criar_db.py
@@ -3,20 +3,32 @@ import sqlite3
 conn = sqlite3.connect('clima_organizacional.db')
 cursor = conn.cursor()
 
+cursor.execute('DROP TABLE resultados')
+
+# Criar a tabela 'usuarios'
 cursor.execute('''
-DROP TABLE respostas
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY
+)
 ''')
 
 # cursor.execute('''
-# CREATE TABLE respostas (
-#     id INTEGER,
-#     data TEXT,
-#     datetime TEXT,
-#     descricao_pergunta TEXT,
-#     resposta REAL,
-#     sugestao TEXT
-# )
-# ''')
+# INSERT INTO usuarios (id) VALUES (?)
+# ''', [(1,), (2,), (3,)])
+
+# Criar a tabela 'RESPOSTAS'
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS respostas (
+    id INTEGER,
+    data TEXT,
+    datetime TEXT,
+    descricao_pergunta TEXT,
+    resposta REAL
+    FOREIGN KEY (id) REFERENCES usuarios (id)
+)
+''')
+
+
 
 conn.commit()
 conn.close()
