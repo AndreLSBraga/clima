b4437a20287d199b4949f43f73d25154f9fefa22
André Braga <andre-az@hotmail.com>
2024-06-11 11:26:32 -0300

    Inicio transformacao para sem ID

--- criar_db.py
+++ criar_db.py
@@ -15,56 +15,56 @@ cursor.execute('DROP TABLE IF EXISTS respostas')
 cursor.execute('DROP TABLE IF EXISTS sugestoes')
 
 # Crie a tabela 'usuarios'
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INTEGER PRIMARY KEY,
-    cargo TEXT,
-    area TEXT,
-    gestor TEXT
-)
-''')
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS usuarios (
+#     id INTEGER PRIMARY KEY,
+#     cargo TEXT,
+#     area TEXT,
+#     gestor TEXT
+# )
+# ''')
 
-# Crie a tabela 'respostas'
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS respostas (
-    id INTEGER,
-    data TEXT,
-    datetime TEXT,
-    descricao_pergunta TEXT,
-    resposta REAL,
-    FOREIGN KEY (id) REFERENCES usuarios (id)
-)
-''')
+# # Crie a tabela 'respostas'
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS respostas (
+#     id INTEGER,
+#     data TEXT,
+#     datetime TEXT,
+#     descricao_pergunta TEXT,
+#     resposta REAL,
+#     FOREIGN KEY (id) REFERENCES usuarios (id)
+# )
+# ''')
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS sugestoes (
-    id INTEGER,
-    area TEXT,
-    data TEXT,
-    datetime TEXT,
-    categoria TEXT,
-    pergunta TEXT,
-    sugestao TEXT,
-    auto_identificacao INT,
-    id_auto_identificacao INT,
-    FOREIGN KEY (id) REFERENCES usuarios (id)
-)
-''')
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS sugestoes (
+#     id INTEGER,
+#     area TEXT,
+#     data TEXT,
+#     datetime TEXT,
+#     categoria TEXT,
+#     pergunta TEXT,
+#     sugestao TEXT,
+#     auto_identificacao INT,
+#     id_auto_identificacao INT,
+#     FOREIGN KEY (id) REFERENCES usuarios (id)
+# )
+# ''')
 
-# Insira alguns registros na tabela 'usuarios'
-usuarios = [
-    (1, 'Gerente', 'Vendas', 'Carlos Silva'),
-    (2, 'Analista', 'TI', 'Ana Costa'),
-    (3, 'Assistente', 'RH', 'Pedro Souza'),
-    (4, 'Supervisor', 'Produção', 'Fernanda Lima'),
-    (5, 'Coordenador', 'Marketing', 'João Pereira'),
-    (6, 'Técnico', 'Manutenção', 'Mariana Ferreira'),
-    (7, 'Consultor', 'Financeiro', 'Rodrigo Oliveira'),
-    (8, 'Desenvolvedor', 'TI', 'Paula Santos'),
-    (9, 'Designer', 'Criação', 'Juliana Almeida'),
-    (10, 'Engenheiro', 'Projetos', 'Lucas Costa')
-]
-cursor.executemany('INSERT INTO usuarios (id, cargo, area, gestor) VALUES (?, ?, ?, ?)', usuarios)
+# # Insira alguns registros na tabela 'usuarios'
+# usuarios = [
+#     (1, 'Gerente', 'Vendas', 'Carlos Silva'),
+#     (2, 'Analista', 'TI', 'Ana Costa'),
+#     (3, 'Assistente', 'RH', 'Pedro Souza'),
+#     (4, 'Supervisor', 'Produção', 'Fernanda Lima'),
+#     (5, 'Coordenador', 'Marketing', 'João Pereira'),
+#     (6, 'Técnico', 'Manutenção', 'Mariana Ferreira'),
+#     (7, 'Consultor', 'Financeiro', 'Rodrigo Oliveira'),
+#     (8, 'Desenvolvedor', 'TI', 'Paula Santos'),
+#     (9, 'Designer', 'Criação', 'Juliana Almeida'),
+#     (10, 'Engenheiro', 'Projetos', 'Lucas Costa')
+# ]
+# cursor.executemany('INSERT INTO usuarios (id, cargo, area, gestor) VALUES (?, ?, ?, ?)', usuarios)
 
 
 
