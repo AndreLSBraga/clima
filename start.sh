f96a1e034b2bce60e419575dfda51b6fd797eb28
André Braga <andre-az@hotmail.com>
2024-07-03 16:03:08 -0300

    Inclusão de logo

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Espere até que o banco de dados esteja pronto
 echo "Aguardando o banco de dados estar disponível..."
 while ! nc -z db 3306; do
+  echo "Esperando pelo banco de dados..."
   sleep 1
 done
 
-# Executa o script de criação do banco de dados (se necessário)
-#python criar_db.py
+echo "Banco de dados disponível. Executando script de criação do banco de dados..."
+python criar_db.py
 
-# Inicia o servidor Flask
-flask run
+echo "Iniciando o servidor Flask..."
+flask run --host=0.0.0.0
